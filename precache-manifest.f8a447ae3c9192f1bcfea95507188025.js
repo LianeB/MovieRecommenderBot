1406b84e984927397182212af605586b2b60e200
Liane <liane.brisebois@usherbrooke.ca>
2021-11-30 00:06:17 -0500

    Updates

---
+++ precache-manifest.f8a447ae3c9192f1bcfea95507188025.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9cc1b7855d8ffa0884aa56deb06b461",
+    "url": "/MovieRecommenderBot/index.html"
+  },
+  {
+    "revision": "f40b38139fe71f0c2365",
+    "url": "/MovieRecommenderBot/static/css/main.b2a06e25.chunk.css"
+  },
+  {
+    "revision": "5ea4ae8542211394f5d8",
+    "url": "/MovieRecommenderBot/static/js/2.9dc09f56.chunk.js"
+  },
+  {
+    "revision": "0def066106b5692cd5c135dcafc5767d",
+    "url": "/MovieRecommenderBot/static/js/2.9dc09f56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f40b38139fe71f0c2365",
+    "url": "/MovieRecommenderBot/static/js/main.5b689e1e.chunk.js"
+  },
+  {
+    "revision": "a192624826da4d574e34",
+    "url": "/MovieRecommenderBot/static/js/runtime-main.8b508be5.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/MovieRecommenderBot/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/MovieRecommenderBot/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/MovieRecommenderBot/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/MovieRecommenderBot/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/MovieRecommenderBot/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/MovieRecommenderBot/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/MovieRecommenderBot/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/MovieRecommenderBot/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "25e2f74d5ce30ec18b482a101504b2a3",
+    "url": "/MovieRecommenderBot/static/media/logo-moviebot-small-circle2.25e2f74d.png"
+  },
+  {
+    "revision": "1b422c1fbed46d1d92f451bd7872094c",
+    "url": "/MovieRecommenderBot/static/media/ratings-demo.1b422c1f.gif"
+  },
+  {
+    "revision": "6a8c0c325d6ffb7ff10b1b7033a49756",
+    "url": "/MovieRecommenderBot/static/media/recommendation-demo.6a8c0c32.gif"
+  },
+  {
+    "revision": "864caac9c7f3933cda758afb709bdb60",
+    "url": "/MovieRecommenderBot/static/media/stats-demo.864caac9.gif"
+  },
+  {
+    "revision": "761c7f939f34bdfce75ff51a1641374a",
+    "url": "/MovieRecommenderBot/static/media/video-placeholder-movieBot2.761c7f93.png"
+  },
+  {
+    "revision": "f3e8d1cec00504428b3c54bec9b75940",
+    "url": "/MovieRecommenderBot/static/media/watchlist-demo.f3e8d1ce.gif"
+  }
+]);
